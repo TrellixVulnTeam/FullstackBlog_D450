321e5a86a82413cfa34d351b0b194350438d7108
dillon32 <dhheyck@gmail.com>
2021-09-15 08:11:41 -0400

    Added first components

---
+++ ng-blog/src/app/article-lists/article-lists.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArticleListsComponent } from './article-lists.component';
+
+describe('ArticleListsComponent', () => {
+  let component: ArticleListsComponent;
+  let fixture: ComponentFixture<ArticleListsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArticleListsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArticleListsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ng-blog/src/app/article-lists/article-lists.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-lists',
+  templateUrl: './article-lists.component.html',
+  styles: [
+  ]
+})
+export class ArticleListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
